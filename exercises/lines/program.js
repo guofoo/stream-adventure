b78936af6ae5afa76f8b088b4553d5a0e8cb944f
E T <guofoo>
2015-04-02 01:21:13 +0000

    commit exercise content by Live4Code

--- exercises/lines/program.js
+++ exercises/lines/program.js
@@ -5,6 +5,7 @@ var split = require('split');
 var lineCount = 0;
 var tr = through(function (buf, _, next) {
     var line = buf.toString();
+    // enter your code below
     this.push(lineCount % 2 === 0
         ? line.toLowerCase() + '\n'
         : line.toUpperCase() + '\n'
@@ -13,5 +14,7 @@ var tr = through(function (buf, _, next) {
     next();
 });
 
-// enter your code below
-
+process.stdin
+    .pipe(split())
+    .pipe(tr)
+    .pipe(process.stdout);
